d2384a3a69c8dca77b39fca2e22f25ccc51fc732
longtb <longtb@younetgroup.com>
2020-07-16 16:06:36 +0700

    + add demo test

---
+++ src/components/ButtonSwitch/tests/demo.test.js
@@ -0,0 +1,31 @@
+/**
+ * Testing ButtonSwitch component
+ *
+ * Run `jest app/components/core/buttonswitch` to test this
+ * Run `jest app/components/core/buttonswitch -u` to update file snapshot
+ *
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+describe('Testing ButtonSwitch', () => {
+    /**
+   * Runs a function before each of the tests in this file runs.
+   * If the function returns a promise or is a generator, Jest waits for that promise to resolve before running the test.
+   */
+    beforeEach(() => {
+
+    });
+
+    /**
+     * Runs a function after each one of the tests in this file completes.
+     * If the function returns a promise or is a generator, Jest waits for that promise to resolve before continuing.
+     */
+    afterEach(() => {
+
+    });
+
+    it('should true equal true', () => {
+        expect(true).toEqual(true);
+    });
+});
